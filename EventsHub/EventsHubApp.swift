59905bbea716b20f88dedbc2fbe473b365bb809c
tataaval <94060509+tataaval@users.noreply.github.com>
2025-12-20 17:26:18 +0400

    first commit

---
+++ EventsHub/EventsHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventsHubApp.swift
+//  EventsHub
+//
+//  Created by Tatarella on 20.12.25.
+//
+
+import SwiftUI
+
+@main
+struct EventsHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
